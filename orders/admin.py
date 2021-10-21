293d2ec00b00eba8c792a8d6dcb2950525523876
lilbunnyfufuu <thegunners.ptd@gmail.com>
2021-10-21 10:36:44 +0700

    add static

--- orders/admin.py
+++
@@ -1,8 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-
-from .models import Order, OrderProduct, Payment
-
-admin.site.register(Order)
-admin.site.register(Payment)
-admin.site.register(OrderProduct)
